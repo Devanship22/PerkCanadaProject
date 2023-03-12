f5883060a0f5a7dbf5425eddfb2fef306aab020f
Devanship22 <dhruvanshi222@gmail.com>
2023-03-12 01:24:40 -0500

    Add project files.

---
+++ PerkCanadaProject/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using PerkCanadaProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace PerkCanadaProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult NewsStories()
+        {
+            try
+            {
+                var url = XDocument.Load("http://www.cbc.ca/cmlink/rss-topstories");
+
+                var newslist = from item in url.Descendants("item")
+                               select new CBCNewsStory
+                               {
+                                   Title = item.Element("title").Value,
+                                   Description = item.Element("description").Value,
+                                   AuthorName = item.Element("author").Value
+                               };
+
+                return View(newslist.ToList());
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
---
+++ PerkCanadaProject/Models/CBCNewsStory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PerkCanadaProject.Models
+{
+    public class CBCNewsStory
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string AuthorName { get; set; }
+        public string PubDate { get; set; }
+
+
+    }
+}
